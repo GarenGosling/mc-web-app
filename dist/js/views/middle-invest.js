d09387bfbde83419ca99acff0cd3e46446125bdf
pro <pro@prodeMacBook-Pro.local>
2017-10-07 22:23:20 +0800

    中产投资弹窗html

---
+++ dist/js/views/middle-invest.js
@@ -0,0 +1,27 @@
+$(function() {
+	var app = new Org({
+		$el: '#middle-invest',
+		events: {
+			'view-detail': function() {
+				orgDetail.show();
+			},
+			'toggle-select': function($this, event) {
+				$this.parent().siblings('ul').toggle();
+			},
+			select: function($this, event) {
+                var txt = $(event.target).html();
+                $this.parent().siblings().find('span').html(txt);
+                $this.parent().hide();
+            }
+		}
+	})
+
+	var orgDetail = new OrgPop({
+		$el: '#tpl-pop-detail-wrap',
+		events: {
+            close: function() {
+                this.hide();
+            }
+        }
+	})
+})
