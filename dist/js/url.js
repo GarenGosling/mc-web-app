e83aef65fa91e45ebd2bbbeb956fc5a40ed4f0da
pro <pro@prodeMacBook-Pro.local>
2017-10-14 10:53:34 +0800

    调用首页轮播接口

--- dist/js/url.js
+++ dist/js/url.js
@@ -1,5 +1,6 @@
 url = {
     host : '',
-    host_source: 'http://120.27.22.41:9091/download/',
-    partner_link : function() {return this.host+'/partnerLink/all';}
+    host_source: 'http://120.27.22.41:9091/download',
+    partner_link : function() {return this.host+'/partnerLink/all';},
+    carousel: function() {return this.host+'/carousel/all';}
 }
--- dist/js/views/index.js
+++ dist/js/views/index.js
@@ -21,14 +21,91 @@ $(function() {
                 if (v !== '') __data[k] = v;
             });
             $.ajax({
-            	url: url.partner_link()
+                url: url.partner_link()
             })
             .done(function(res) {
-                console.log(res)
                 _this.data.host_source = url.host_source;
                 _this.data.list = res.data;
                 _this.$render();
             })
+        }
+    });
+
+    var orgList2 = new Org({
+        $el: '#index',
+        $renderEl: '#banner',
+        $tpl: '#tpl-banner',
+        data: {
+            searchData: {
+            },
+            list: []
+        },
+        events: {},
+        init: function() {
+            this.getList();
+        },
+        getList: function(data) {
+            var _this = this;
+            var _data = $.extend({}, this.data.searchData, data);
+            var __data = {};
+            $.each(_data, function(k, v) {
+                if (v !== '') __data[k] = v;
+            });
+            $.ajax({
+                url: url.carousel()
+            })
+            .done(function(res) {
+                _this.data.host_source = url.host_source;
+                _this.data.list = res.data;
+                _this.$render();
+                _this.setBanner();
+            })
+        },
+        setBanner: function() {
+            setTimeout(function() {
+                $('#banner').slide({
+                    picIndex: 0
+                });
+            }, 100)
+        }
+    });
+
+    var orgList3 = new Org({
+        $el: '#index',
+        $renderEl: '#banner2',
+        $tpl: '#tpl-banner2',
+        data: {
+            searchData: {
+            },
+            list: []
+        },
+        events: {},
+        init: function() {
+            this.getList();
+        },
+        getList: function(data) {
+            var _this = this;
+            var _data = $.extend({}, this.data.searchData, data);
+            var __data = {};
+            $.each(_data, function(k, v) {
+                if (v !== '') __data[k] = v;
+            });
+            $.ajax({
+            	url: url.carousel()
+            })
+            .done(function(res) {
+                _this.data.host_source = url.host_source;
+                _this.data.list = res.data;
+                _this.$render();
+                _this.setBanner();
+            })
+        },
+        setBanner: function() {
+            setTimeout(function() {
+                $('#banner2').slide({
+                    picIndex: 0
+                });
+            }, 100)
         }
     });
 })
