dd6dba8f1246f1df0a05ca2d1148c367ad0a726a
pro <pro@prodeMacBook-Pro.local>
2017-10-24 21:35:37 +0800

    页面样式修改

--- dist/js/views/elite-model-list.js
+++ dist/js/views/elite-model-list.js
@@ -2,47 +2,7 @@ $(function() {
 	var app = new Org({
 		$el: '#elite-model-list',
 		events: {
-			'add-collect': function() {
-				orgCollect.show();
-			}
 		}
 	})
 
-	var orgCollect = new OrgPop({
-		$el: '#tpl-pop-collect-wrap',
-		events: {
-			'hide-select': function() {
-				$('.j-body-hide').hide();
-			},
-            close: function() {
-                this.hide();
-            },
-			'toggle-select': function($this, event) {
-				var e = event || window.event;
-				if(e.stopPropagation) {
-					e.stopPropagation();
-				} else if(e.cancelBubble) {
-					e.cancelBubble = true;
-				}
-				$this.parent().siblings('ul').toggle();
-			},
-			select: function($this, event) {
-                var txt = $(event.target).html();
-                $this.parent().siblings().find('span').html(txt);
-                $this.parent().hide();
-            },
-			'add-clip': function() {
-                orgClip.show();
-            }
-        }
-	})
-
-	var orgClip = new OrgPop({
-		$el: '#tpl-pop-clip-wrap',
-		events: {
-            close: function() {
-                this.hide();
-            }
-        }
-	})
 })
