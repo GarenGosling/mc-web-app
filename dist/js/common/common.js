430733f49fcb49933e2feedc1cb8948582e38adf
GarenGosling <liuxueliang@garen.org>
2017-11-04 03:30:04 +0800

    文章列表

--- dist/js/common/common.js
+++ dist/js/common/common.js
@@ -36,6 +36,7 @@ url.mc.articles = server.mc + '/articles';
 url.mc.moreAttention_all=server.mc+"/moreAttentions";
 url.mc.lifeWizard_status=server.mc+"/lifeWizard/status";
 url.host.index = server.host;
+url.host.articles_queryByAuthor = server.mc + "/api/articles/queryByAuthor";
 
 
 /***************** 登录相关 *******************/
